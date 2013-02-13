176ba8b13542c1938959c46b013b28d8a470f257
Codeopolis <lucas@nuxbox.me>
2013-02-13 13:37:36 -0500

    Added web.config and AjaxControlToolkit

--- NorthBay.Web/Global.asax.cs
+++ NorthBay.Web/Global.asax.cs
@@ -3,9 +3,7 @@ using System.Globalization;
 using System.Threading;
 using System.Web;
 using System.Web.Security;
-using NorthBay.Framework.Authentication;
 using NorthBay.Logic.User;
-using NorthBay.Utility;
 using NorthBay.Utility.Configuration;
 
 namespace NorthBay.Web
@@ -27,6 +25,11 @@ namespace NorthBay.Web
             Thread.CurrentThread.CurrentCulture = cultureInfo;
         }
 
+        /// <summary>
+        /// Checks for user role and to restrict access to folders
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         protected void FormsAuthentication_OnAuthenticate(Object sender, FormsAuthenticationEventArgs e)
         {
             UserClass userClass = new UserClass();
