2b2d4d09543394b4c9428107f8020325dccd7d11
jay3dave <jay3dave@gmail.com>
2013-04-10 21:43:54 -0400

    Image Gallery Web Commit

---
+++ NorthBay.Web/ImageGallery/Default.aspx.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using NorthBay.Logic.ImageGallery;
+using NorthBay.Framework.Database;
+using NorthBay.Utility;
+
+namespace NorthBay.Web.ImageGallery
+{
+    public partial class Default : Basepage
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            ImageGalleryClass objImgGal = new ImageGalleryClass();
+            GridView1.DataSource = objImgGal.getgallery();
+            GridView1.DataBind();
+        }
+    }
+}
